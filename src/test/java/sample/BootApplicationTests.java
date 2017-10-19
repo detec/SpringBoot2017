ba1f66426dd03a29b3cdf3dec91954746382f575
WINMAIN\Andrii <andrii.duplyk@nitka.com>
2017-10-19 10:09:16 +0300

    Added Junit 5

--- src/test/java/sample/BootApplicationTests.java
+++ src/test/java/sample/BootApplicationTests.java
@@ -1,11 +1,14 @@
 package sample;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class BootApplicationTests {
 
